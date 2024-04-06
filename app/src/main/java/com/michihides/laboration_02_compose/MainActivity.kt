3655c42020750eb4fd20c117766b999abbe0d2de
michihidesugito <Michihides@gmail.com>
2024-04-06 21:05:06 +0200

    added navigation logic

--- app/src/main/java/com/michihides/laboration_02_compose/MainActivity.kt
+++ app/src/main/java/com/michihides/laboration_02_compose/MainActivity.kt
@@ -3,14 +3,22 @@ package com.michihides.laboration_02_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.michihides.laboration_02_compose.destinations.AboutScreenDestination
+import com.michihides.laboration_02_compose.destinations.SignInScreenDestination
+import com.michihides.laboration_02_compose.destinations.SignUpScreenDestination
 import com.michihides.laboration_02_compose.ui.theme.Laboration_02_ComposeTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +30,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
     }
 }
+
+@RootNavGraph(start = true)
+@Destination
+@Composable
+fun Home(navigator: DestinationsNavigator) {
+    Column {
+        Text(text = "HomeScreen")
+
+        Button(onClick = {
+            navigator.navigate(SignInScreenDestination)
+        }) {
+            Text(text = "Sign In")
+        }
+
+        Button(onClick = {
+            navigator.navigate(SignUpScreenDestination)
+        }) {
+            Text(text = "Sign Up")
+        }
+
+        Button(onClick = {
+            navigator.navigate(AboutScreenDestination)
+        }) {
+           Text(text = "About Us")
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/michihides/laboration_02_compose/ui/screens/SignUpScreen.kt
@@ -0,0 +1,23 @@
+package com.michihides.laboration_02_compose.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.michihides.laboration_02_compose.destinations.HomeDestination
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Destination
+@Composable
+fun SignUpScreen(navigator: DestinationsNavigator) {
+    Column {
+        Text(text = "Sign Up Page")
+
+        Button(onClick = {
+            navigator.navigate(HomeDestination)
+        }) {
+            Text(text = "Back to Home")
+        }
+    }
+}
