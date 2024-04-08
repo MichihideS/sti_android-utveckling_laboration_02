979fef089642d6faf3883b6d9afec7f421ffd88c
michihidesugito <Michihides@gmail.com>
2024-04-08 13:18:04 +0200

    added alert dialog with predefined array of users

--- app/src/main/java/com/michihides/laboration_02_compose/ui/screens/SignInScreen.kt
+++ app/src/main/java/com/michihides/laboration_02_compose/ui/screens/SignInScreen.kt
@@ -1,12 +1,19 @@
 package com.michihides.laboration_02_compose.ui.screens
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.michihides.laboration_02_compose.R
 import com.michihides.laboration_02_compose.destinations.HomeDestination
 import com.michihides.laboration_02_compose.ui.composables.ButtonColumn
@@ -15,6 +22,9 @@ import com.michihides.laboration_02_compose.ui.errorHandling.LoginErrorHandler
 import com.michihides.laboration_02_compose.ui.composables.LoginHandler
 import com.michihides.laboration_02_compose.ui.composables.TextColumn
 import com.michihides.laboration_02_compose.ui.models.User
+import com.michihides.laboration_02_compose.ui.theme.BeigeDark
+import com.michihides.laboration_02_compose.ui.theme.BeigeTransparency
+import com.michihides.laboration_02_compose.ui.theme.WhiteDarker
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -26,6 +36,34 @@ fun SignInScreen(navigator: DestinationsNavigator) {
     )
 
     ButtonColumn {
+        val showArray = remember { mutableStateOf(false) }
+
+        if(showArray.value) {
+            AlertDialog(
+                onDismissRequest = { showArray.value = false },
+                title = { Text(text = stringResource(id = R.string.alert_dialog_title))},
+                text = { Text(
+                    text = stringResource(id = R.string.alert_dialog_users),
+                    fontSize = 16.sp
+                )},
+                shape = RoundedCornerShape(5.dp),
+                containerColor = WhiteDarker,
+                confirmButton = {
+                    Button(
+                        onClick = {showArray.value = false},
+                        colors = ButtonDefaults.buttonColors(BeigeDark),
+                        shape = RoundedCornerShape(5.dp)
+                        ) {
+                        Text(text = stringResource(id = R.string.alert_dialog_confirm))
+                    }
+                }
+            )
+        }
+
+        GeneralButton(textButton = stringResource(id = R.string.predefined_array)) {
+                showArray.value = true
+        }
+        
         var user by remember {
             mutableStateOf(
                 User("", "")
